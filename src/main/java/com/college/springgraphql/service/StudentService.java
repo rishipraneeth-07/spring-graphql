161e19e49a0084e138f8604c613fadf2f43889ba
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-07-26 19:30:31 +0530

    feat:add Student Service interface

---
+++ src/main/java/com/college/springgraphql/service/StudentService.java
@@ -0,0 +1,12 @@
+package com.college.springgraphql.service;
+
+import com.college.springgraphql.dto.StudentInput;
+import com.college.springgraphql.entity.Student;
+
+import java.util.List;
+
+public interface StudentService {
+    Student createStudent(StudentInput  studentInput);
+    Student getStudentById(Long id);
+    List<Student> getAllStudents();
+}

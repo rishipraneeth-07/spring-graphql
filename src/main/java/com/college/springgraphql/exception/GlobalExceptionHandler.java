2330778df0d45e92d04a6236c768b8adb2e7f82e
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-07-26 22:15:52 +0530

    feat:Add Global exceptional Handler for graphql

---
+++ src/main/java/com/college/springgraphql/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.college.springgraphql.exception;
+
+import graphql.ErrorType;
+import graphql.GraphQLError;
+import graphql.GraphqlErrorBuilder;
+import org.springframework.graphql.data.method.annotation.GraphQlExceptionHandler;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @GraphQlExceptionHandler
+    public GraphQLError handleStudentNotFoundException(StudentNotFoundException e) {
+        return GraphqlErrorBuilder.newError()
+                .message(e.getMessage())
+                .errorType(ErrorType.DataFetchingException)
+                .build();
+    }
+}

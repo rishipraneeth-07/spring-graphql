e83414ea0de44a3315ecab8647ae5c3f0baede79
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-07-27 19:17:27 +0530

    feat:Implemented StudentService

--- src/main/java/com/college/springgraphql/service/impl/StudentServiceImpl.java
+++ src/main/java/com/college/springgraphql/service/impl/StudentServiceImpl.java
@@ -1,4 +1,46 @@
 package com.college.springgraphql.service.impl;
 
-public class StudentServiceImpl {
+import com.college.springgraphql.dto.StudentInput;
+import com.college.springgraphql.entity.Student;
+import com.college.springgraphql.exception.EmailAlreadyExistsException;
+import com.college.springgraphql.exception.StudentNotFoundException;
+import com.college.springgraphql.repository.StudentRepository;
+import com.college.springgraphql.service.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StudentServiceImpl implements StudentService {
+    private final StudentRepository studentRepository;
+    @Override
+    public Student createStudent(StudentInput studentInput) {
+        Student existingStudent = studentRepository.findByEmail(studentInput.getEmail());
+
+        if (existingStudent != null) {
+            throw new EmailAlreadyExistsException(
+                    "Email already exists: " + studentInput.getEmail());
+        }
+        Student student = new Student();
+        student.setName(studentInput.getName());
+        student.setAge(studentInput.getAge());
+        student.setEmail(studentInput.getEmail());
+        student.setCgpa(studentInput.getCgpa());
+        Student savedStudent=studentRepository.save(student);
+        return  savedStudent;
+    }
+
+    @Override
+    public Student getStudentById(Long id) {
+        return studentRepository.findById(id).orElseThrow(
+                ()->new StudentNotFoundException("Student not found"));
+
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
 }

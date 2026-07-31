838493663d1b1e9611ad74d3e99d9b38075f9e8e
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-07-31 22:27:28 +0530

    feat:replaced list getAllStudents to page in StudentService

--- src/main/java/com/college/springgraphql/service/StudentService.java
+++ src/main/java/com/college/springgraphql/service/StudentService.java
@@ -1,6 +1,7 @@
 package com.college.springgraphql.service;
 
 import com.college.springgraphql.dto.StudentInput;
+import com.college.springgraphql.dto.StudentPage;
 import com.college.springgraphql.entity.Student;
 
 import java.util.List;
@@ -8,7 +9,7 @@ import java.util.List;
 public interface StudentService {
     Student createStudent(StudentInput  studentInput);
     Student getStudentById(Long id);
-    List<Student> getAllStudents();
+    StudentPage getAllStudents(int page, int size);
     Student updateStudent(Long id, StudentInput studentInput);
     boolean deleteStudentById(Long id);
 }

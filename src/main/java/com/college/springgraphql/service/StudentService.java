ef53198f5c4a86268a279a7f453ec10dbe39c988
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-08-03 21:38:29 +0530

    feat: add sorting parameters to student service interface

--- src/main/java/com/college/springgraphql/service/StudentService.java
+++ src/main/java/com/college/springgraphql/service/StudentService.java
@@ -3,13 +3,20 @@ package com.college.springgraphql.service;
 import com.college.springgraphql.dto.StudentInput;
 import com.college.springgraphql.dto.StudentPage;
 import com.college.springgraphql.entity.Student;
+import com.college.springgraphql.enums.SortDirection;
+import com.college.springgraphql.enums.StudentSortField;
 
 import java.util.List;
 
 public interface StudentService {
     Student createStudent(StudentInput  studentInput);
     Student getStudentById(Long id);
-    StudentPage getAllStudents(int page, int size);
+    StudentPage getAllStudents(
+            int page,
+            int size,
+            StudentSortField sortBy,
+            SortDirection direction
+    );
     Student updateStudent(Long id, StudentInput studentInput);
     boolean deleteStudentById(Long id);
 }

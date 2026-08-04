f528a691e4f8796aeff734e755552cf6a58907f1
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-08-04 23:23:10 +0530

    feat: add department entity

---
+++ src/main/java/com/college/springgraphql/entity/Department.java
@@ -0,0 +1,27 @@
+package com.college.springgraphql.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "departments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String hod;
+
+    @Column(nullable = false)
+    private String building;
+}

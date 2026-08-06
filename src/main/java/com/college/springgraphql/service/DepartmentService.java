7318bf5e25374fa4e1d71b966266cba3b8fcb6ef
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-08-06 19:24:36 +0530

    feat: add department service interface

--- src/main/java/com/college/springgraphql/service/DepartmentService.java
+++ src/main/java/com/college/springgraphql/service/DepartmentService.java
@@ -1,4 +1,14 @@
 package com.college.springgraphql.service;
 
-public class DepartmentService {
+import com.college.springgraphql.dto.DepartmentInput;
+import com.college.springgraphql.dto.DepartmentResponse;
+
+import java.util.List;
+
+public interface DepartmentService {
+    DepartmentResponse createDepartment(DepartmentInput departmentInput);
+    DepartmentResponse getDepartmentById(Long id);
+    List<DepartmentResponse> getAllDepartments();
+    DepartmentResponse updateDepartment(Long id, DepartmentInput departmentInput);
+    boolean deleteDepartmentById(Long id);
 }
